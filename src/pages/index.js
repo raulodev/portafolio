44c06fe47fbd7e1282940c2862662fd96c1b17b8
raulodev <raulodev@gmail.com>
2023-04-29 13:04:20 -0400

    centrar texto de la vista principal

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import Image from "next/image";
-
 import { CustomHead } from "@/components/Head";
 
 export default function Home() {
@@ -10,20 +9,19 @@ export default function Home() {
       <main className="min-h-screen selection:bg-green-500 selection:text-white">
         <div className="grid grid-cols-1 xl:grid-cols-2">
           <div className="flex items-center justify-center min-h-screen bg-zinc-800">
-            <div className="w-3/4 space-y-5 sm:w-2/3 xl:3/4">
+            <div className="w-3/4 space-y-5 text-center xl:text-left sm:w-2/3 xl:3/4 ">
               <div className="text-5xl font-semibold text-white uppercase">Hola!</div>
               <div className="text-6xl font-bold text-white uppercase">
                 Yo soy <span className="text-green-500 ">Raul</span>
               </div>
-
               <div>
                 <p className="text-lg text-white">
                   Un desarrollador full-stack que busca constantemente nuevos retos y oportunidades
                 </p>
               </div>
-              <div className="mt-5">
+              <div className="flex justify-center mt-5">
                 <Link href="/about">
-                  <div className="px-5 py-3 text-xl font-bold text-white uppercase transition-shadow bg-green-500 rounded-full shadow-lg hover:shadow w-max">
+                  <div className="px-5 py-3 text-xl font-bold text-white uppercase transition-all ease-in-out bg-green-600 rounded-full shadow-lg hover:bg-green-500 hover:shadow w-max">
                     Sobre mi
                   </div>
                 </Link>
